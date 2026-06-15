d36393546b5d71ebbb651ca3743f2227f3b8be82
Artur Koshtei <gh.public10110@gmail.com>
2026-06-15 05:55:35 +0300

    fix

--- src/types/respjs.d.ts
+++ src/types/respjs.d.ts
@@ -1,5 +1,6 @@
 declare module 'respjs' {
   // Minimal typing to satisfy usages in adapter without pulling in external types.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const Resp: any
   export default Resp
 }
